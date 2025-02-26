5362e72b0b847e728bcd2fb86516d41d5d5c560f
Akash <codeguyakash@gmail.com>
2025-02-26 09:46:49 +0530

    i🚀

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -5,6 +5,9 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.json({ message: "Server Running" });
+});
 app.get("/api/hello", (req, res) => {
     res.json({ message: "Hello from Express on Netlify!" });
 });
